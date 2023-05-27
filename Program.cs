7b4fa81d03977b472e458c104e9e0b15721bc01f
Gabrielcpellanda <gabrielpellanda@gmail.com>
2023-05-26 23:44:31 -0300

    Configurações do App alteradas

--- Program.cs
+++ Program.cs
@@ -1,6 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Beredicas online");
+var connectionString
+= app.Configuration.GetConnectionString("DefaultConnection");
+
+var secrets = new Secrets();
+app.Configuration.GetSection("Secrets").Bind(secrets);
+
+app.MapGet("/", () => new
+{
+    ConnectionString = connectionString,
+    Secrets = secrets
+
+});
 
 app.Run();
+
+public class Secrets
+{
+    public string JwtKey { get; set; } = string.Empty;
+
+    public string ApiKeyName { get; set; } = string.Empty;
+
+    public string ApiKey { get; set; } = string.Empty;
+}
